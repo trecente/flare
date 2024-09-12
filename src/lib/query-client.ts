62764584997ac38d7f5db884b6272462859233fb
Lucas Trecente <lucastrecente@hotmail.com>
2024-09-12 02:40:24 -0300

    refactor(query-client): adjust staleTime and disable retries

--- src/lib/query-client.ts
+++ src/lib/query-client.ts
@@ -3,8 +3,9 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
+      staleTime: 60 * 60 * 1000, // 1 hour
       refetchOnWindowFocus: false,
+      retry: false,
     },
   },
 });
