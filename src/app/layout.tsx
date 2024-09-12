528a4d8fbf96dbf47da534043f3aeed1accf0e56
Lucas Trecente <lucastrecente@hotmail.com>
2024-09-12 02:42:03 -0300

    feat(layout): add toaster component and update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { Providers } from "@/app/providers";
 import { BackToTop } from "@/components/back-to-top";
 import { Header } from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./globals.css";
 
@@ -11,9 +12,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Flare",
+    template: "Flare | %s",
     default: "Flare",
   },
+  description:
+    "Browse through a wide collection of anime images and easily find what you’re looking for by filtering by tags, artists, characters, and age ratings.",
 };
 
 export default function RootLayout({
@@ -28,6 +31,7 @@ export default function RootLayout({
           <Header />
           {children}
           <BackToTop />
+          <Toaster />
         </Providers>
       </body>
     </html>
