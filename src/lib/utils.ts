c60da41c6d2d3306ae43f30d689e1df0f7be61ad
Lucas Trecente <lucastrecente@hotmail.com>
2025-02-10 19:36:01 -0300

    refactor: remove old filtering system

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,35 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { ageRatings } from "@/constants/age-rating";
-import { FilterCategory, FilterOption, FilterType } from "@/types/filter";
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function appendValuesToParams(
-  params: URLSearchParams,
-  key: string,
-  values: (string | number)[]
-): void {
-  values.forEach((value) => params.append(key, value.toString()));
-}
-
-export function createCategory(
-  name: string,
-  type: FilterType,
-  options: FilterOption[]
-): FilterCategory {
-  return {
-    name,
-    type,
-    options,
-  };
-}
-
-export function getNamesByIds(ageRatingIds: number[]): string[] {
-  return ageRatingIds
-    .map((id) => ageRatings.find((r) => r.id === id)?.name.toLowerCase())
-    .filter(Boolean) as string[];
-}
+export const capitalize = (str: string): string =>
+  str.charAt(0).toUpperCase() + str.slice(1);
